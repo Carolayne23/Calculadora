ea223207446bdb0070944a2cfe9624f2ced892ff
Carolayne23 <140124163+Carolayne23@users.noreply.github.com>
2025-03-25 10:47:51 -0500

    Add files via upload

--- base-calculadora-curso-js-master/script.js
+++ base-calculadora-curso-js-master/script.js
@@ -1,15 +1,19 @@
-function agregarALaPantalla(value) {
-    document.getElementById('pantalla').value += value;
+
+//esto agrega a la pantalla el valor que se le pase
+function agregar(valor) {
+    document.getElementById('pantalla').value += valor;
 }
 
+
+//esto calcula el valor que se le pase
 function calcular() {
     const valorPantalla = document.getElementById('pantalla').value;
     const result = eval(valorPantalla);
     document.getElementById('pantalla').value = result;
 }
 
-function limpiarPantalla() {
+
+//esto borra el valor que se le pase
+function borrar() {
     document.getElementById('pantalla').value = '';
 }
-
-
